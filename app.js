bd61682138dd0e81a1624ef6c64ef9c3f9f75247
Adam Lang <adam.lang-1@hotmail.com>
2021-04-05 15:49:50 +0200

    feat: npm init, node modules and lodash

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-const http = require('http');
+const _ = require('lodash');
 
-const server = http.createServer((request, response) => {
-    if (request.url === '/') {
-        response.end('Welcome to our homepage');
-    } else if (request.url === '/about') {
-        response.end('Here is our short history');
-    } else {
-        response.end(`
-        <h1>Oops!</h1>
-        <p>We can't seem to find the page you're looking for</p>
-        <a href="/">Back Home<a/>
-        `);
-    }
-});
-
-server.listen(5000);
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);

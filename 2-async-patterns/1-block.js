0bf573021fcd54ceb20659663c1ea2c6cfd97e20
Adam Lang <adam.lang-1@hotmail.com>
2021-04-06 11:04:52 +0200

    feat: readfile, writefile async promise

--- 2-async-patterns/1-block.js
+++
@@ -1,9 +0,0 @@
-const { readFile } = require('fs');
-
-readFile('./content/first.txt', 'utf-8', (error, response) => {
-    if (error) {
-        return;
-    } else {
-        console.log(response);
-    }
-});
--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-const { readFile } = require('fs');
+// SAME AS FILE 9 IN FOLDER 1 BUT WITH PROMISES TO AVOID CALLBACK HELL
+const { readFile, writeFile } = require('fs').promises;
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
-readFile('./content/first.txt', 'utf-8', (error, response) => {
-    if (error) {
-        return;
-    } else {
-        console.log(response);
+const start = async () => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf-8');
+        const second = await readFile('./content/second.txt', 'utf-8');
+        await writeFile(
+            './content/result-async-promise',
+            `This is awesome: ${first} ${second}`,
+            { flag: 'a' }
+        );
+        console.log(first, second);
+    } catch (error) {
+        console.log(error);
     }
-});
+};
+start();
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf-8', (error, response) => {
+//             if (error) {
+//                 reject(error);
+//             } else {
+//                 resolve(response);
+//             }
+//         });
+//     });
+// };
+// getText('./content/first.txt')
+//     .then((result) => console.log(result))
+//     .catch((error) => console.log(error));
